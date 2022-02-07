86a7a924b5545c5550e171b867afe37f00ef376b
Carl <cboettig@gmail.com>
2022-02-07 22:49:42 +0000

    try-2

--- training-A2C.py
+++ training-A2C.py
@@ -3,6 +3,8 @@ import gym_climate
 import gym_conservation
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
 
 # Parallel environments
 env = make_vec_env("ays-v0", n_envs=4)
@@ -11,3 +13,4 @@ model = A2C("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=1000)
 
 model.save("ays-v0-A2C-carl")
+score = evaluate_policy(model, Monitor(env), n_eval_episodes=10)
